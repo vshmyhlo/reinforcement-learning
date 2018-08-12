5683f7545597067be3d7f87dafda44c84379ba1c
Vlad Shmyhlo <aion.stu@gmail.com>
2018-08-12 13:26:14 +0300

    Cleanup

--- utils.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-
-def select_action_value(value, action):
-    num_samples = tf.shape(value)[0]
-    indices = tf.stack([tf.range(0, tf.cast(num_samples, action.dtype)), action], -1)
-
-    return tf.gather_nd(value, indices)
