ea2aa3b912cb3e2aaad96e9b67ce8a64501b1b1c
Vlad Shmyhlo <aion.stu@gmail.com>
2020-01-20 16:59:19 +0200

    Update

--- test/test_vec_env.py
+++ test/test_vec_env.py
@@ -1,60 +1,72 @@
 import numpy as np
+import torch
+
+from utils import n_step_discounted_return
 from vec_env import VecEnv
-
-
-class ObservationSpace(object):
-    def __init__(self):
-        self.shape = (1,)
-
-
-class ActionSpace(object):
-    def __init__(self):
-        self.shape = (1,)
-
-    def sample(self):
-        return [-1]
 
 
 class SampleEnv(object):
     def __init__(self):
-        self.observation_space = ObservationSpace()
-        self.action_space = ActionSpace()
+        self.observation_space = object()
+        self.action_space = object()
         self.i = None
 
     def reset(self):
         self.i = 0
 
-        return ['abc'[self.i]]
+        return 'abc'[self.i]
 
     def step(self, a):
-        assert a.shape == self.action_space.shape
+        self.i += a
 
-        self.i += a[0]
+        return 'abc'[self.i], a * 10, self.i >= 2, None
 
-        return ['abc'[self.i]], a[0] * 10, self.i >= 2, None
+    @staticmethod
+    def value(s):
+        return {
+            'a': 20,
+            'b': 10,
+            'c': 0,
+        }[s]
 
 
 def test_vec_env():
     env = VecEnv([lambda: SampleEnv() for _ in range(3)])
 
+    history = []
     s = env.reset()
-
-    assert np.array_equal(s, [['a'], ['a'], ['a']])
+    assert np.array_equal(s, ['a', 'a', 'a'])
 
     # step 1
-    a = np.array([[0], [1], [2]])
+    a = np.array([0, 1, 2])
     s_prime, r, d, _ = env.step(a)
+    history.append((r, d))
 
-    assert np.array_equal(s_prime, [['a'], ['b'], ['a']])
-    assert np.array_equal(r, [0, 10, 20])
+    assert np.array_equal(s_prime, ['a', 'b', 'a'])
+    assert np.array_equal(r, [0., 10., 20.])
     assert np.array_equal(d, [False, False, True])
 
     # step 2
-    a = np.array([[0], [1], [2]])
+    a = np.array([0, 1, 2])
     s_prime, r, d, _ = env.step(a)
+    history.append((r, d))
 
-    assert np.array_equal(s_prime, [['a'], ['a'], ['a']])
-    assert np.array_equal(r, [0, 10, 20])
+    assert np.array_equal(s_prime, ['a', 'a', 'a'])
+    assert np.array_equal(r, [0., 10., 20.])
     assert np.array_equal(d, [False, True, True])
 
     env.close()
+
+    r, d = zip(*history)
+    r = torch.tensor(r, dtype=torch.float).transpose(0, 1)
+    d = torch.tensor(d, dtype=torch.bool).transpose(0, 1)
+
+    v = torch.tensor([SampleEnv.value(s) for s in s_prime])
+    actual = n_step_discounted_return(r, v, d, 0.9)
+    expected = torch.tensor([
+        [16.2, 18.],
+        [19., 10.],
+        [20., 20.],
+    ])
+
+    assert torch.allclose(actual, expected)
